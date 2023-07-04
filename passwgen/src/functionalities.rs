a9085b0ea2161e79bf8e323d5dfdd544cff727f6
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-07-04 17:30:38 -0600

    Adding more functionalities

--- passwgen/src/functionalities.rs
+++ passwgen/src/functionalities.rs
@@ -25,3 +25,55 @@ fn fill_data() {
         }
     }
 }
+
+pub fn generate_save_safety_password(app_name: &str) {
+
+    let rand_password: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect();
+
+    let mut file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(true)
+        .open(FILE_NAME)
+        .unwrap();
+
+    if let Err(e) = writeln!(file, "{},{}", app_name, rand_password) {
+        eprintln!("Couldn't write to file: {}", e);
+    }
+
+    fill_data();
+
+    println!("Password generated and saved for the application: {}", app_name);
+}
+
+pub fn show_list_applications() {
+    fill_data();
+    let application_data = APPLICATION_DATA.lock().unwrap();
+
+    for (key, value) in application_data.iter() {
+        print!("Application_Name: {}\n", key);
+        print!("Password: {}\n", value);
+        println!();
+    }
+}
+
+pub fn get_password_for_specify_app(app_name: &str) {
+    fill_data();
+    let application_data = APPLICATION_DATA.lock().unwrap();
+
+    let to_find = [app_name];
+    for &name_app in &to_find {
+        match application_data.get(name_app) {
+            Some(password) => {
+                print!("Application_Name: {}\n", name_app);
+                print!("Password: {}\n", password);
+                println!();
+            },
+            None => println!("{name_app} don't exists.")
+        }
+    }
+}
