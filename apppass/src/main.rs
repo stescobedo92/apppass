b221f540693a04d4d411c10ad2b3e5f62f3144ff
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2025-01-10 11:58:52 -0600

    Adding new functionalities

--- apppass/src/main.rs
+++ apppass/src/main.rs
@@ -1,50 +1,97 @@
 mod functionalities;
 
-use clap::{App, Arg };
+use clap::{Arg, ArgAction, Command};
 
 fn main() {
-    let apppass = App::new("apppass")
-        .version("1.0")
+    let apppass = Command::new("apppass")
+        .version("1.1")
         .author("Sergio Triana Escobedo")
         .about("Generate secure passwords for your applications.")
         .arg(
-            Arg::with_name("app")
-                .short("a")
+            Arg::new("app")
+                .short('a')
                 .long("app")
-                .value_name("APP_NAME")
-                .help("Application name")
-                .takes_value(true)
-                .required(false),
+                .action(ArgAction::Set)
+                .help("Generate a password for an application"),
         )
         .arg(
-            Arg::with_name("list")
-                .short("l")
+            Arg::new("length")
+                .short('n')
+                .long("length")
+                .action(ArgAction::Set)
+                .help("Password length"),
+        )
+        .arg(
+            Arg::new("list")
+                .short('l')
                 .long("list")
-                .help("Show list of applications")
-                .required(false),
+                .action(ArgAction::SetTrue)
+                .help("List all applications"),
         )
         .arg(
-            Arg::with_name("get")
-                .short("g")
+            Arg::new("get")
+                .short('g')
                 .long("get")
-                .value_name("APP_NAME")
-                .takes_value(true)
-                .help("Get data passing an application name")
-                .required(false),
+                .action(ArgAction::Set)
+                .help("Get password for an application"),
+        )
+        .arg(
+            Arg::new("delete")
+                .short('d')
+                .long("delete")
+                .action(ArgAction::Set)
+                .help("Delete an application"),
+        )
+        .arg(
+            Arg::new("update")
+                .short('u')
+                .long("update")
+                .action(ArgAction::Set)
+                .help("Update password for an application"),
+        )
+        .arg(
+            Arg::new("export")
+                .long("export")
+                .action(ArgAction::Set)
+                .help("Export passwords to CSV"),
+        )
+        .arg(
+            Arg::new("import")
+                .long("import")
+                .action(ArgAction::Set)
+                .help("Import passwords from CSV"),
         )
         .get_matches();
 
-    if apppass.is_present("app") {
-        let name = apppass.value_of("app").unwrap();
-        functionalities::generate_save_safety_password(name);
+    if let Some(name) = apppass.get_one::<String>("app") {
+        let length = apppass
+            .get_one::<String>("length")
+            .and_then(|l| l.parse::<usize>().ok());
+        functionalities::generate_save_safety_password(name, length);
     }
 
-    if apppass.is_present("list") {
+    if *apppass.get_one::<bool>("list").unwrap_or(&false) {
         functionalities::show_list_applications();
     }
 
-    if apppass.is_present("get") {
-        let name = apppass.value_of("get").unwrap();
+    if let Some(name) = apppass.get_one::<String>("get") {
         functionalities::get_password_for_specify_app(name);
+    }
+
+    if let Some(name) = apppass.get_one::<String>("delete") {
+        functionalities::delete_password(name);
+    }
+
+    if let Some(name) = apppass.get_one::<String>("update") {
+        let new_pass = "new_secure_password";
+        functionalities::update_password(name, new_pass);
+    }
+
+    if let Some(path) = apppass.get_one::<String>("export") {
+        functionalities::export_passwords(path);
+    }
+
+    if let Some(path) = apppass.get_one::<String>("import") {
+        functionalities::import_passwords(path);
     }
 }
